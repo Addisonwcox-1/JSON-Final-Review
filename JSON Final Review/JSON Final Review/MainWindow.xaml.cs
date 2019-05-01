a08fecde21e13ab2077b12da74a0a551393b7829
Cox, Addison W <Addison.W.Cox-1@ou.edu>
2019-05-01 11:26:28 -0500

    Adding sthuff

---
+++ JSON Final Review/JSON Final Review/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Final_Review
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var movie = client.GetAsync($"http://pcbstuou.w27.wh-2.com/webservices/3033/api/Movies?number=100").Result;
+                if (movie.IsSuccessStatusCode)
+                {
+                    var movieResult = movie.Content.ReadAsStringAsync().Result;
+                    Movie movieType = JsonConvert.DeserializeObject<Movie>(movieResult);
+                }
+
+            }
+        }
+    }
+}
---
+++ JSON Final Review/JSON Final Review/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace JSON_Final_Review
+{
+    internal class Movie
+    {
+        public int movieID { get; set; }
+        public string directorName { get; set; }
+        public int numCriticReviews { get; set; }
+        public int duration { get; set; }
+        public int gross { get; set; }
+        public string genre { get; set; }
+        public string actorName { get; set; }
+        public string movieTitle { get; set; }
+        public int numVotes { get; set; }
+    }
+}
